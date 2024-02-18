a67c0c903b43ddb46294d5241693953b4109725a
r0naldo007 <arpitpansuriya007@gmail.com>
2024-02-18 11:10:16 -0500

    frontend register page done by gurleen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,22 @@
-import logo from "./logo.svg";
+import { Container } from "react-bootstrap";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import RegisterPage from "./pages/RegisterPage";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Router>
+        <main className="py-3">
+          <Container>
+            <Routes>
+              <Route path="/" element={<RegisterPage />} />
+            </Routes>
+          </Container>
+        </main>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
---
+++ frontend/src/store.js
@@ -0,0 +1,24 @@
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import { thunk } from "redux-thunk";
+
+import { composeWithDevTools } from "redux-devtools-extension/developmentOnly"; // Change the import path
+
+import { userRegisterReducer } from "./reducers/user";
+
+const reducer = combineReducers({
+  userRegister: userRegisterReducer,
+});
+
+const initialState = {
+  userLogin: {},
+};
+
+const middleware = [thunk];
+
+const store = createStore(
+  reducer,
+  initialState,
+  composeWithDevTools(applyMiddleware(...middleware))
+);
+
+export default store;
