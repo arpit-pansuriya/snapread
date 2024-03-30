3e95655c196426526db2d4522a51da0c1b4e1e12
r0naldo007 <arpitpansuriya007@gmail.com>
2024-03-30 16:43:05 -0400

    payment integration

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,9 @@ const userRoutes = require("./routes/user.js");
 const productRoutes = require("./routes/product.js");
 
 const orderRoutes = require("./routes/order.js");
-
+const uploadRoutes = require("./routes/upload.js");
+const paymentRoutes = require("./routes/payment.js");
+const uploadRoutes = require("./routes/upload.js");
 dotenv.config();
 
 connectDB();
@@ -18,9 +20,25 @@ connectDB();
 const app = express();
 
 // Routes
-app.use("/api/users", userRoutes);
 app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
 app.use("/api/orders", orderRoutes);
+app.use("/api/payment", paymentRoutes);
+app.use("/api/upload", uploadRoutes);
+
+const __dirname = path.resolve();
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running...");
+  });
+}
 
 app.use(notFound);
 app.use(errorHandler);
